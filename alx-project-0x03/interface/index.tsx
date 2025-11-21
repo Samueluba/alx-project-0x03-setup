51a2b1b5588a2d494e462c8112d48a89af673eb8
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-21 13:35:36 +0100

    Update index.tsx

--- alx-project-0x03/interface/index.tsx
+++ alx-project-0x03/interface/index.tsx
@@ -1,1 +1,15 @@
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
+  action?: () => void;
+}
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface PageRouteProps {
+  pageRoute: string;
+}
 
